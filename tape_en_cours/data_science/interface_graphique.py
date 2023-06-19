26ae11c334cd419861b0f85ae746207caef1a04a
Matthieu Falce <matthieu@falce.net>
2023-06-19 14:40:20 +0200

    gitautosync update 62

--- tape_en_cours/data_science/interface_graphique.py
+++ tape_en_cours/data_science/interface_graphique.py
@@ -6,3 +6,8 @@ Created on Mon Jun 19 14:39:30 2023
 @author: mfalce
 """
 
+from tkinter import filedialog 
+from tkinter import *
+Tk().withdraw() 
+filename = filedialog.askopenfilename(initialdir= "/",title="Select File",filetypes=(("png files","*.png"),("jpeg files","*.jpg"),("all files","*.*"))) 
+print(filename)
