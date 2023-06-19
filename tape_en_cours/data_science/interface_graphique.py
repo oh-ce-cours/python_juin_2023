c3e5e89956f4af66d764b24fd25f0e45d8c9a46a
Matthieu Falce <matthieu@falce.net>
2023-06-19 14:42:20 +0200

    gitautosync update 67

--- tape_en_cours/data_science/interface_graphique.py
+++ tape_en_cours/data_science/interface_graphique.py
@@ -12,6 +12,6 @@ Tk().withdraw()
 filename = filedialog.askopenfilename(
     initialdir= ".",
     title="Select File",
-    filetypes=(("all files",".*"))
+    filetypes=(("csv","*.csv"))
 ) 
 print(filename)
