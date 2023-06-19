a614a982d3a7f4847da52d1b3d79722cf34a49cf
Matthieu Falce <matthieu@falce.net>
2023-06-19 15:40:53 +0200

    gitautosync update 101

--- tape_en_cours/data_science/interface_graphique.py
+++ tape_en_cours/data_science/interface_graphique.py
@@ -56,33 +56,3 @@ canvas = FigureCanvasTkAgg(fig, master=root)  # A tk.DrawingArea.
 canvas.draw()
 canvas.get_tk_widget().pack(side=tkinter.TOP, fill=tkinter.BOTH, expand=1)
 root.mainloop()
-
-
-
-
-
-import numpy as np
-
-
-root = tkinter.Tk()
-root.wm_title("Embedding in Tk")
-
-t = np.arange(0, 3, .01)
-fig.add_subplot(111).plot(t, 2 * np.sin(2 * np.pi * t))
-
-
-canvas.mpl_connect("key_press_event", on_key_press)
-
-
-def _quit():
-    root.quit()     # stops mainloop
-    root.destroy()  # this is necessary on Windows to prevent
-                    # Fatal Python Error: PyEval_RestoreThread: NULL tstate
-
-
-button = tkinter.Button(master=root, text="Quit", command=_quit)
-button.pack(side=tkinter.BOTTOM)
-
-tkinter.mainloop()
-# If you put root.destroy() here, it will cause an error if the window is
-# closed with the window manager.
