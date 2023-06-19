f9bbc217fa4da09f8bd8c7f7028754dcf85e15d1
Matthieu Falce <matthieu@falce.net>
2023-06-19 14:55:04 +0200

    gitautosync update 73

--- tape_en_cours/data_science/interface_graphique.py
+++ tape_en_cours/data_science/interface_graphique.py
@@ -25,16 +25,5 @@ def get_filename() -> str:
 turn_on = Button(root, text="ON", command=get_filename)
 turn_on.pack()
 
-turn_off = Button(root, text="OFF", command=root.quit)
-turn_off.pack()
-
-volume = Label(root, text="VOLUME")
-volume.pack()
-
-vol_up = Button(root, text="+")
-vol_up.pack()
-
-vol_down = Button(root, text="-")
-vol_down.pack()
 
 root.mainloop()
