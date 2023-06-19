54a386d797fea01133253685af7621c6ebd4bdd5
Matthieu Falce <matthieu@falce.net>
2023-06-19 12:35:59 +0200

    gitautosync update 46

--- tape_en_cours/data_science/analyse_csv.py
+++ tape_en_cours/data_science/analyse_csv.py
@@ -9,4 +9,4 @@ Created on Mon Jun 19 12:17:17 2023
 import pandas as pd 
 
 fname = "TX_comb_3p63GHZ_deuxdents_shift2p3GHz_AWG_deuxfiltresIFDMS.CSV"
-df = pd.read_csv(fname, sep=";", skiprows=31, columns=["freq", "db"])
+df = pd.read_csv(fname, sep=";", skiprows=31, column=["freq", "db"])
