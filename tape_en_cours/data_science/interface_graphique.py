bd84b77f33eb1a7d8376c1d5db40a955c7a31086
Matthieu Falce <matthieu@falce.net>
2023-06-19 14:49:09 +0200

    gitautosync update 71

--- tape_en_cours/data_science/interface_graphique.py
+++ tape_en_cours/data_science/interface_graphique.py
@@ -8,7 +8,8 @@ Created on Mon Jun 19 14:39:30 2023
 
 from tkinter import filedialog 
 from tkinter import *
-Tk().withdraw() 
+
+root = Tk()  # create parent window
 
 
 def get_filename() -> str:
@@ -19,3 +20,21 @@ def get_filename() -> str:
         ) 
     return filename
 
+
+# use Button and Label widgets to create a simple TV remote
+turn_on = Button(root, text="ON")
+turn_on.pack()
+
+turn_off = Button(root, text="OFF", command=root.quit)
+turn_off.pack()
+
+volume = Label(root, text="VOLUME")
+volume.pack()
+
+vol_up = Button(root, text="+")
+vol_up.pack()
+
+vol_down = Button(root, text="-")
+vol_down.pack()
+
+root.mainloop()
